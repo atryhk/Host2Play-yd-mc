5000e7d0551aa1646457cb4e66ec72eeea17f56d
atryhk <crazyatry@gmail.com>
2026-06-29 22:01:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 # 配置区域
 # ==============================================================================
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=004d0672-3aed-4426-a8e7-e62f79f1df18",
     # 添加更多链接
 ]
 
